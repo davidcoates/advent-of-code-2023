0350de475f4dde55d42ba9f584a95b0bb3052db7
David <davidcoates95@gmail.com>
2023-12-02 22:14:26 +1100

    [Day2] polish

--- Day2/Part2.hs
+++ Day2/Part2.hs
@@ -2,12 +2,12 @@ import Day2
 
 import qualified Data.Map as Map
 
-power :: [Draw] -> Int
-power draws = maxNum Red * maxNum Green * maxNum Blue where
+power :: Game -> Int
+power (_, draws) = maxNum Red * maxNum Green * maxNum Blue where
   maxNum color = maximum $ Map.findWithDefault 0 color <$> draws
 
 main :: IO ()
 main = do
   games <- parseGames
-  let answer = sum [ power draws | (_, draws) <- games ]
+  let answer = sum $ power <$> games
   putStrLn $ show answer
